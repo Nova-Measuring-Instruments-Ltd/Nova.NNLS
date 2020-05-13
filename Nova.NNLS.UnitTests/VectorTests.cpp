f31905d643c4188f7f0e1841b3ea67d512e2b9ca
nova-inna-w <61831242+nova-inna-w@users.noreply.github.com>
2020-05-13 23:43:21 +0300

    Add tests for vector and matrix construction with external data

--- Nova.NNLS.UnitTests/VectorTests.cpp
+++ Nova.NNLS.UnitTests/VectorTests.cpp
@@ -4,6 +4,8 @@
 
 #include "vector.h"
 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
 namespace NNLSUnitTests
 {
 	TEST_CLASS(VectorTests)
@@ -13,5 +15,22 @@ namespace NNLSUnitTests
 		{
 			nsNNLS::vector vec;
 		}
+
+		TEST_METHOD(VectorConstructionWithExternalData)
+		{
+			double externalData[] = { 1, 2, 3 };
+			const size_t length = 3;
+			
+			nsNNLS::vector vec(3, externalData);
+			double* actualData = vec.getData();
+
+			Assert::AreEqual(length, vec.length());
+			Assert::AreEqual(static_cast<double*>(externalData), actualData);
+			for(int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(externalData[i], actualData[i]);
+			}
+		}
+
 	};
 }
